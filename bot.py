0b2071fc4488808099ab3ede7d5ba5ff0f9f70da
ShiyadChathoth <113101977+ShiyadChathoth@users.noreply.github.com>
2025-07-31 18:55:16 +0530

    modified_telegram_bot.py

--- bot.py
+++ bot.py
@@ -21,7 +21,7 @@ import unicodedata
 
 # Set up logging
 logging.basicConfig(
-    format=\'%(asctime)s - %(name)s - %(levelname)s - %(message)s\',
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
 )
 logger = logging.getLogger(__name__)
